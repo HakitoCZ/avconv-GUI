d6fe21aafc617b4fdd71a15a819a359cda96777b
HakitoCZ <Hakito.cz@gmail.com>
2013-08-25 23:48:03 +0200

    module os removed, print-output

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Description
 
 python command line interface for avconv
 
+To properly working the python program, you shall have installed avconv or ffmpeg - avconv is now part of ffmpeg package so don't be affraid.
+
 License
 -------
 
--- avconv-cli.py
+++ avconv-cli.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 
 import glob
-import os
 import subprocess
 
 song_list = glob.glob('*.mp3')
 
 for x in song_list:
     print(x)
+    print(x[:-3] + 'm4a')
 
-os.mkdir('converted')
-
-subprocess.call('for i in *.mp3; do avconv -i "${i}" -b 94k "${i%.mp3}.m4a"; done', shell=True)
-subprocess.call('mv *.m4a converted', shell=True) 
+for i in song_list:
+    print('avconv', '-i', i, '-b', '96k', i[:-3] + 'm4a')
 
