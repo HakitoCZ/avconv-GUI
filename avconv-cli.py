b812114c0d794d75679ecb9185a219d92b927bfe
HakitoCZ <Hakito.cz@gmail.com>
2013-08-25 17:02:01 +0200

    added module subprocess

--- avconv-cli.py
+++ avconv-cli.py
@@ -2,6 +2,7 @@
 
 import glob
 import os
+import subprocess
 
 song_list = glob.glob('*.mp3')
 
@@ -9,4 +10,7 @@ for x in song_list:
     print(x)
 
 os.mkdir('converted')
+
+subprocess.call('for i in *.mp3; do avconv -i "${i}" -b 94k "${i%.mp3}.m4a"; done', shell=True)
+subprocess.call('mv *.m4a converted', shell=True) 
 
