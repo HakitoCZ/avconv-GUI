f2e1160e5a5cb1e4958807a03ea6ffba29a8a13a
HakitoCZ <Hakito.cz@gmail.com>
2013-08-28 23:05:03 +0200

    working subprocess and preparation for moving files

--- avconv-cli.py
+++ avconv-cli.py
@@ -2,14 +2,21 @@
 
 import glob
 import subprocess
+import os
+
 
 song_list = glob.glob('*.mp3')
 
-for x in song_list:
-    print(x)
-    print(x[:-3] + 'm4a')
+#for x in song_list:
+    #print(x)
+    #print(x[:-3] + 'm4a')
 
 for i in song_list:
-    print('avconv', '-i', i, '-b', '96k', i[:-3] + 'm4a')
+    subprocess.call(['avconv', '-i', i, '-b', '96k', i[:-3] + 'm4a'])
 
-# print for subprocess.Popen
+
+#song_list_converted = glob.glob('*.m4a')
+
+#os.mkdir('converted')
+#subprocess.call(['mv', song_list_converted, 'converted/'])
+
