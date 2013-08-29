9b46c9db9eb328c956b5f2de0a9ddb0af82ed443
HakitoCZ <Hakito.cz@gmail.com>
2013-08-29 11:03:02 +0200

    creating dir and moving files

--- avconv-cli.py
+++ avconv-cli.py
@@ -7,16 +7,9 @@ import os
 
 song_list = glob.glob('*.mp3')
 
-#for x in song_list:
-    #print(x)
-    #print(x[:-3] + 'm4a')
+os.mkdir('converted')
 
 for i in song_list:
     subprocess.call(['avconv', '-i', i, '-b', '96k', i[:-3] + 'm4a'])
-
-
-#song_list_converted = glob.glob('*.m4a')
-
-#os.mkdir('converted')
-#subprocess.call(['mv', song_list_converted, 'converted/'])
+    subprocess.call(['mv', i[:-3] + 'm4a', 'converted'])
 
