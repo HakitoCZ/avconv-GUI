724d4b73a6291744a3887cf81324c9e010a65b65
HakitoCZ <Hakito.cz@gmail.com>
2013-08-29 12:12:08 +0200

    improved moving converted files

--- avconv-cli.py
+++ avconv-cli.py
@@ -10,6 +10,5 @@ song_list = glob.glob('*.mp3')
 os.mkdir('converted')
 
 for i in song_list:
-    subprocess.call(['avconv', '-i', i, '-b', '96k', i[:-3] + 'm4a'])
-    subprocess.call(['mv', i[:-3] + 'm4a', 'converted'])
+    subprocess.call(['avconv', '-i', i, '-b', '96k', 'converted/' + i[:-3] + 'm4a'])
 
