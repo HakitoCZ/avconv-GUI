d54eb91eeee5d9d1ad53e613c87134e239c1a69d
HakitoCZ <Hakito.cz@gmail.com>
2013-08-31 11:05:19 +0200

    variables improved

--- avconv-cli.py
+++ avconv-cli.py
@@ -5,19 +5,17 @@ import subprocess
 import os
 
 
-bitrate_input = input('\nHello, this script will convert mp3 songs in current directory to m4a audio format.\nMake sure in current dir is no folder called "converted". If it is, move it elsewhere or rename it.\nIf there already are some m4a files in current directory, move them.\n\nNow set number of bitrate, in kbps. If you want to use default, insert 0: ')
+bitrate_input = int(input('\nHello, this script will convert mp3 songs in current directory to m4a audio format.\nMake sure in current dir is no folder called "converted". If it is, move it elsewhere or rename it.\nIf there already are some m4a files in current directory, move them.\n\nNow set number of bitrate, in kbps. If you want to use default, insert 0: '))
 
 song_list = glob.glob('*.mp3')
 
 os.mkdir('converted')
 
-bitrate = int(bitrate_input)
-
-if bitrate == 0:
-    bitrate = 96
-elif bitrate < 0:
+if bitrate_input == 0:
+    bitrate_input = 96
+elif bitrate_input < 0:
     print('Bitrate have to be more than 0!')
 
 for i in song_list:
-    subprocess.call(['avconv', '-i', i, '-b', str(bitrate) + 'k', 'converted/' + i[:-3] + 'm4a'])
+    subprocess.call(['avconv', '-i', i, '-b', str(bitrate_input) + 'k', 'converted/' + i[:-3] + 'm4a'])
 
