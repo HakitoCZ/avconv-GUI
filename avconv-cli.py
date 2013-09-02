af3cb254faf29cb4acfe77c9b5d3d5becb9709b5
HakitoCZ <Hakito.cz@gmail.com>
2013-09-02 21:37:02 +0200

    If statements working properly

--- avconv-cli.py
+++ avconv-cli.py
@@ -10,14 +10,12 @@ print('\nHello, this script will convert mp3 songs in current directory to reque
 print('Make sure in current dir is no folder called "converted". If it is, move it elsewhere or rename it.')
 print('If there already are some files in requested format in current directory, move them.')
 
-output_file = int(input('\nNow select requested output format:\n1 - M4A\n2 - OGG\n3 - WAV\n4 - AAC\n4 - MP3\n'))
+output_file = int(input('\nNow select requested output format:\n1 - M4A\n2 - OGG\n3 - WAV\n4 - AAC\n5 - MP3\n'))
 
-bitrate_input = int(input('\nNow set number of bitrate, in kbps. If you want to use default, insert 0\n'))
 
 
 song_list = glob.glob('*.mp3')
 
-os.mkdir('converted')
 
 
 if output_file == 1:
@@ -26,11 +24,17 @@ elif output_file == 2:
     out = 'ogg'
 elif output_file == 3:
     out = 'wav'
+    print('wav')
 elif output_file == 4:
+    out = 'aac'
+elif output_file == 5:
     out = 'mp3'
-else:
-     print('You inserted unknown request, run program and try it again.')
+elif output_file > 5:
+    print('You inserted unknown request, run program and try it again.')
 
+os.mkdir('converted')
+
+bitrate_input = int(input('\nNow set number of bitrate, in kbps. If you want to use default, insert 0\n'))
 
 if bitrate_input == 0:
     bitrate_input = 96
