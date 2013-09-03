61692db124c6b28a35246c095e4e2559a2404898
HakitoCZ <Hakito.cz@gmail.com>
2013-09-03 12:31:41 +0200

    improved bitrate input, added counting mp3 files

--- avconv-cli.py
+++ avconv-cli.py
@@ -8,10 +8,18 @@ import sys
 
 print('\nHello, this script will convert mp3 songs in current directory to requested audio format.')
 print('Make sure in current dir is no folder called "converted". If it is, move it elsewhere or rename it.')
-print('If there already are some files in requested format in current directory, move them.')
+print('If there already are some files in requested format in current directory, move them.\n')
 
 
 song_list = glob.glob('*.mp3')
+
+
+
+print('There is', len(song_list), 'mp3 files in current directory.')
+
+if len(song_list) < 1:
+    sys.exit('I cannot work without mp3 files in current directory.')
+
 
 
 output_file = int(input('\nNow select requested output format:\n1 - M4A\n2 - OGG\n3 - WAV\n4 - AAC\n5 - MP3\n'))
@@ -22,27 +30,39 @@ elif output_file == 2:
     out = 'ogg'
 elif output_file == 3:
     out = 'wav'
-    print('wav')
 elif output_file == 4:
     out = 'aac'
 elif output_file == 5:
     out = 'mp3'
-elif output_file > 5:
-    print('You inserted unknown request, run program and try it again.')
-
-
-bitrate_input = int(input('\nNow set number of bitrate, in kbps. If you want to use default, insert 0\n'))
-
-if bitrate_input == 0:
-    bitrate_input = 96
-elif bitrate_input < 0:
-    print('Bitrate have to be more than 0!')
 else:
-     print('You inserted unknown request, run program and try it again.')
+    sys.exit('You inserted unknown request, run program and try it again.')
+
+print('Working on', out)
+
+
+
+bitrate_input = int(input('\nSelect requested quality:\n1 - Very high (256 kb/s)\n2 - High (128 kb/s)\n3 - Average (96 kb/s)\n4 - Poor (64 kb/s)\n'))
+
+if bitrate_input == 1:
+    bit = 256
+elif bitrate_input == 2:
+    bit = 128
+elif bitrate_input == 3:
+    bit = 96
+elif bitrate_input == 4:
+    bit = 64
+else:
+    sys.exit('You inserted unknown request, run program and try it again.')
+
+print('Selected quality is', bit, 'kb/s')
+
 
 
 os.mkdir('converted')
 
-for i in song_list:
-    subprocess.call(['avconv', '-i', i, '-b', str(bitrate_input) + 'k', 'converted/' + i[:-3] + out])
+print('Creating directory "converted"')
 
+for i in song_list:
+    subprocess.call(['avconv', '-i', i, '-b', bit + 'k', 'converted/' + i[:-3] + out])
+
+print('Done, converted files can be found in "converted" directory.')
